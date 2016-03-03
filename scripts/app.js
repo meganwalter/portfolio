8a5a5b5dbefc1dfb6c5be1c58f7bd4516b15265d
Megan Walter <megwalter@hotmail.com>
2016-03-03 00:57:31 -0500

    added the tohtml prototype method and continued working on my js to print articles to the dom

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+var myProjs = [];
+
+function Project(articleObj) {
+  this.name = articleObj.name;
+  this.pubdate = articleObj.pubdate;
+  this.body = articleObj.body;
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.find('h1').html(this.name);
+  $newProject.find('time[pubdate]').attr('datetime', this.pubdate);
+  $newProject.find('.article-body').html(this.body);
+  $newProject.append('<hr>');
+  $newProject.removeClass('template');
+  return $newProject;
+};
+
+projects.forEach(function(obj)) {
+  myProjs.push(new Project(obj));
+});
+
+myProjs.forEach(function(a) {
+  $('#articles').append(a.toHtml());
+});
