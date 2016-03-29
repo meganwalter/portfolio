1406972ab92dab95db1ca14ee556c03080775f6c
Megan Walter <megwalter@hotmail.com>
2016-03-28 23:48:24 -0400

    added some code to populate a skills section, trying to add a map and reduce function

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,60 @@
     this.body = articleObj.body;
   }
 
+  function Skills(data) {
+    this.skill = data.skill;
+    this.years = data.years;
+  }
+
+  Skills.all = [];
+
+  Skills.prototype.render = function() {
+    var skillTemp = $('#skills-template').html();
+    var compileSkill = Handlebars.compile(skillTemp);
+    return compileSkill(this);
+  };
+
+  Skills.print = function(data) {
+    data.forEach(function(obj) {
+      Skills.all.push(new Skills(obj));
+    });
+
+    Skills.all.forEach(function(b) {
+      $('#skills-page').append(b.render());
+    });
+
+  };
+
+  Skills.months = function() {
+    Skills.all.map(function(skill) {
+      console.log(skill.years);
+      return skill.years;
+    }).reduce(function(months, year) {
+      months.push(year * 12);
+      console.log(months);
+      return months;
+    }, []);
+  };
+
+  Skills.months();
+
+  Skills.getSkills = function() {
+    console.log("this works");
+    if (localStorage.skills) {
+      Skills.print(JSON.parse(localStorage.skills));
+    } else {
+      $.getJSON('../data/sample.json', function(data) {
+        localStorage.setItem('skills', JSON.stringify(data));
+        Skills.print(data);
+        console.log(data);
+      }).fail(function() {
+        console.log('fail');
+      }).done(function() {
+        console.log('done');
+      });
+    }
+  }
+
   Project.all = [];
 
   Project.prototype.toHtml = function() {
@@ -44,4 +98,5 @@
   };
 
   module.Project = Project;
+  module.Skills = Skills;
 })(window);
