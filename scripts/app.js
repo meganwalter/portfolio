e1a60b8876c4a4456ed622381eaee75b0c5bfc48
Megan Walter <megwalter@hotmail.com>
2016-03-28 22:04:53 -0400

    put the js files into IIFEs

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,47 @@
-function Project(articleObj) {
-  this.name = articleObj.name;
-  this.pubdate = articleObj.pubdate;
-  this.img = articleObj.img;
-  this.body = articleObj.body;
-}
+(function(module) {
 
-Project.all = [];
-
-Project.prototype.toHtml = function() {
-  var projectTemp = $('#project-template').html();
-  var compiledTemp = Handlebars.compile(projectTemp);
-  return compiledTemp(this);
-};
-
-Project.loadAll = function(projects) {
-
-  projects.forEach(function(obj) {
-    Project.all.push(new Project(obj));
-  });
-  Project.all.forEach(function(a) {
-    $('#article-view').append(a.toHtml());
-  });
-};
-
-Project.fetchAll = function() {
-  console.log("this works");
-  if (localStorage.projects) {
-    Project.loadAll(JSON.parse(localStorage.projects));
-  } else {
-    $.getJSON('../data/articles.json', function(data) {
-      localStorage.setItem('projects', JSON.stringify(data));
-      Project.loadAll(data);
-      console.log(data);
-    }).fail(function(){
-      console.log('fail');
-    }).done(function() {
-      console.log('done');
-    });
+  function Project(articleObj) {
+    this.name = articleObj.name;
+    this.pubdate = articleObj.pubdate;
+    this.img = articleObj.img;
+    this.body = articleObj.body;
   }
-};
+
+  Project.all = [];
+
+  Project.prototype.toHtml = function() {
+    var projectTemp = $('#project-template').html();
+    var compiledTemp = Handlebars.compile(projectTemp);
+    return compiledTemp(this);
+  };
+
+  Project.loadAll = function(projects) {
+
+    projects.forEach(function(obj) {
+      Project.all.push(new Project(obj));
+    });
+
+    Project.all.forEach(function(a) {
+      $('#article-view').append(a.toHtml());
+    });
+  };
+
+  Project.fetchAll = function() {
+    console.log("this works");
+    if (localStorage.projects) {
+      Project.loadAll(JSON.parse(localStorage.projects));
+    } else {
+      $.getJSON('../data/articles.json', function(data) {
+        localStorage.setItem('projects', JSON.stringify(data));
+        Project.loadAll(data);
+        console.log(data);
+      }).fail(function() {
+        console.log('fail');
+      }).done(function() {
+        console.log('done');
+      });
+    }
+  };
+
+  module.Project = Project;
+})(window);
